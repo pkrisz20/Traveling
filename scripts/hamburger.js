07984a20aa085b7e76e3f2334b8364353dc1f3e9
pkrisz20 <81801859+pkrisz20@users.noreply.github.com>
2021-09-20 15:42:23 +0200

    Fixing delete

--- scripts/hamburger.js
+++
@@ -1,7 +0,0 @@
-const menuicon = document.querySelector('.hamburger');
-const navbar = document.querySelector('.navbar');
-
-menuicon.addEventListener('click', () => {
-    navbar.classList.toggle('change');
-    menuicon.classList.toggle('open-menu');
-})
